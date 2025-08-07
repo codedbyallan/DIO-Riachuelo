3ee80b3cadc5e373f85ad417d464892a14400da3
allanbarbosa01 <crash171@gmail.com>
2025-08-07 02:15:41 -0300

    feat: resolver exercício de tabuada com estrutura de repetição for

---
+++ src/Exercicios/B_CondicionaisELaços/Exercicio01/Ex01.java
@@ -0,0 +1,17 @@
+package Exercicios.B_CondicionaisELaços.Exercicio01;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        System.out.println("### Tabuada ###\n");
+        System.out.println("Digite o número da tabuada que você deseja: ");
+        var numero = sc.nextInt();
+        System.out.println("A tabuada do " + numero + " é: ");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(numero + " * " + i + " = " + (numero * i));
+        }
+        sc.close();
+    }
+}
